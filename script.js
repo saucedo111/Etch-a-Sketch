55517c42c1ff86fcaa63f7e9b5e030bc7a15e9a4
saucedo111 <99573171+saucedo111@users.noreply.github.com>
2023-10-05 15:59:41 -0400

    project done, RGB extra next

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('#reset');
 
-function drawGrid(num) {
-    num = 256
+let num = 0;
+function drawGrid() {
     for (let x = 0; x < num; x++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add('cell');
+        newDiv.style.width = 100/ Math.sqrt(num) + '%';
+        newDiv.style.height = 100/ Math.sqrt(num) + '%';
         container.appendChild(newDiv);
     }
 }
+let cellList = document.querySelectorAll('.cell');
+function addColor() {
+    cellList = document.querySelectorAll('.cell');
+    cellList.forEach(function (cell) {
+        cell.addEventListener('mouseover', function (e) {
+            e.target.style.backgroundColor = 'black';
+        });
 
-drawGrid(1)
-
-const cellList = document.querySelectorAll('.cell');
-
-cellList.forEach(function (cell) {
-    cell.addEventListener('mouseover', function (e) {
-        e.target.style.backgroundColor = 'black';
     });
+}
+
+resetButton.addEventListener('click', function (e) {
+    let i = 0;
+    while(i < num) {
+        container.removeChild(container.lastChild);
+        i++;
+    }
+    let x = prompt('How many squares per side?');
+    num = x * x;
+    drawGrid();
+    addColor();
 
 });
+
+
